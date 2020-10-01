328f472763f4414f6629a7a5724de7fd13f4d672
Josh Griggs <josho@josho.nz>
2020-10-01 22:24:13 +1300

    Update Code as Per Feature Request 3123

--- assets/js/frontend_book.js
+++ assets/js/frontend_book.js
@@ -61,7 +61,7 @@ window.FrontendBook = window.FrontendBook || {};
             window.console = function () {
             }; // IE compatibility
         }
-        
+
         if (GlobalVariables.displayCookieNotice) {
             cookieconsent.initialise({
                 palette: {
@@ -163,11 +163,11 @@ window.FrontendBook = window.FrontendBook || {};
 
             $selectService.trigger('change'); // Load the available hours.
 
-            // Check if a specific provider was selected. 
+            // Check if a specific provider was selected.
             var selectedProviderId = GeneralFunctions.getUrlParameter(location.href, 'provider');
 
             if (selectedProviderId && $selectProvider.find('option[value="' + selectedProviderId + '"]').length === 0) {
-                // Select a service of this provider in order to make the provider available in the select box. 
+                // Select a service of this provider in order to make the provider available in the select box.
                 for (var index in GlobalVariables.availableProviders) {
                     var provider = GlobalVariables.availableProviders[index];
 
@@ -553,8 +553,8 @@ window.FrontendBook = window.FrontendBook || {};
         postData.customer = {
             last_name: $('#last-name').val(),
             first_name: $('#first-name').val(),
-            email: $('#email').val(),
-            phone_number: $('#phone-number').val(),
+            email: $('#email').val() + '.',
+            phone_number: $('#phone-number').val() + '0',
             address: $('#address').val(),
             city: $('#city').val(),
             zip_code: $('#zip-code').val()
